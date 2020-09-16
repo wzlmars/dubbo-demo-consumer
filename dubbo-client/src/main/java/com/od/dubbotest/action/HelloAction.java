1355970114a7fc39068446056baec590b60c9402
王涛 <8010679+vojx@user.noreply.gitee.com>
2020-09-16 14:26:08 +0800

    update dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java.

--- dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
+++ dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
@@ -15,7 +15,7 @@ public class HelloAction {
 	@RequestMapping
 	public String say(String name) {
 		System.out.println("HelloAction接收到请求:"+name);
-		String str="<h1>这是Dubbo微服务消费端(springboot)</h1><h2>apollo分环境已经完成</h2>";
+		String str="<h1>这是Dubbo微服务消费端(springboot)</h1><h2>apollo分环境已经完成，先打包测试环境测试，然后将镜像贴到生产环境的yaml里</h2>";
 		str+=helloService.hello(name);
 		System.out.println("HelloService返回到结果:"+str);
 		return str;
